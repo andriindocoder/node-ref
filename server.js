53032d05bb3d063792d0631d5ec4bcc943259278
Andri Sedania <andri@sedaniainnovator.com>
2020-02-05 14:21:24 +0700

    18.219 - Calculator Setup

--- server.js
+++ server.js
@@ -5,14 +5,6 @@ app.get("/", (req, res) => {
 	res.send("<h3>Hello World</h3>");
 });
 
-app.get("/contact", (req, res) => {
-	res.send("Contact me at: andri@sedania.com");
-});
-
-app.get("/about", (req, res) => {
-	res.send("Hi I am Andri. I am great.");
-});
-
 app.listen(3000, function() {
 	console.log("Server started on PORT 3000");
 });
