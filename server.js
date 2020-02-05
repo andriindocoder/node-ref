7f3ddc376d4b22232a83707937a4f4dd955ca879
Andri Sedania <andri@sedaniainnovator.com>
2020-02-05 13:34:56 +0700

    18.214 - Handling Requests and Responses: the GET request

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+	res.send("<h3>Hello World</h3>");
+});
+
 app.listen(3000, function() {
 	console.log("Server started on PORT 3000");
 });
