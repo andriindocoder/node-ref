d135074caf07526537b0b5c41e63ab459ba9bd21
Andri Sedania <andri@sedaniainnovator.com>
2020-05-06 19:55:29 +0700

    Visual Coded Prettify

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-	res.sendFile(__dirname + "/index.html");
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.post("/", (req, res) => {
-	let num1 = Number(req.body.num1);
-	let num2 = Number(req.body.num2);
+app.post('/', (req, res) => {
+  let num1 = Number(req.body.num1);
+  let num2 = Number(req.body.num2);
 
-	let result = num1 + num2;
+  let result = num1 + num2;
 
-	res.send("The Result : " + result);
+  res.send('The Result : ' + result);
 });
 
-app.listen(3000, function() {
-	console.log("Server started on PORT 3000");
+app.listen(3000, function () {
+  console.log('Server started on PORT 3000');
 });
