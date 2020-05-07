236244195444445aa3d282c9e525af3e5ecfaf97
Andri Sedania <andri@sedaniainnovator.com>
2020-05-07 10:19:36 +0700

    19.254 - Using Express to Render a Website with Live API data
    19.255 - Using Body Parser to Parse POST request to server

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
 const express = require('express');
 const https = require('https');
+const bodyParser = require('body-parser');
 
 const app = express();
-const url =
-  'https://api.openweathermap.org/data/2.5/weather?q=Bandung&appid=3cbfea738d409eb1692515dd7947cf7b&units=metric';
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', (req, res) => {
+  const query = req.body.cityName;
+  const apiKey = '3cbfea738d409eb1692515dd7947cf7b';
+  const units = 'metric';
+  const url =
+    'https://api.openweathermap.org/data/2.5/weather?q=' +
+    query +
+    '&appid=' +
+    apiKey +
+    '&units=' +
+    units;
+
   https.get(url, (response) => {
     response.on('data', function (data) {
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.feels_like;
       const weatherDescription = weatherData.weather[0].description;
-      console.log(weatherDescription);
+      const icon = weatherData.weather[0].icon;
+      const imageURL = 'http://openweathermap.org/img/wn/' + icon + '@2x.png';
+      res.write('<p>The weather is currently: ' + weatherDescription + '</p>');
+      res.write(
+        '<h3>The temperature in ' +
+          query +
+          ' is : ' +
+          temp +
+          ' degrees Celcius.</h3>'
+      );
+      res.write('<img src=' + imageURL + '>');
+      res.send();
     });
   });
-
-  res.send('Server is up and running');
 });
 
 app.listen(3000, () => {
