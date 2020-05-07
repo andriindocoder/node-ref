cbc16dfea64d0885c1417972584aa490debcec67
Andri Sedania <andri@sedaniainnovator.com>
2020-05-07 10:48:59 +0700

    19.256 - The Mailchimp API What you’ll make

--- app.js
+++ app.js
@@ -1,46 +1,22 @@
 const express = require('express');
-const https = require('https');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
+
+app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/signup.html');
 });
 
 app.post('/', (req, res) => {
-  const query = req.body.cityName;
-  const apiKey = '3cbfea738d409eb1692515dd7947cf7b';
-  const units = 'metric';
-  const url =
-    'https://api.openweathermap.org/data/2.5/weather?q=' +
-    query +
-    '&appid=' +
-    apiKey +
-    '&units=' +
-    units;
-
-  https.get(url, (response) => {
-    response.on('data', function (data) {
-      const weatherData = JSON.parse(data);
-      const temp = weatherData.main.feels_like;
-      const weatherDescription = weatherData.weather[0].description;
-      const icon = weatherData.weather[0].icon;
-      const imageURL = 'http://openweathermap.org/img/wn/' + icon + '@2x.png';
-      res.write('<p>The weather is currently: ' + weatherDescription + '</p>');
-      res.write(
-        '<h3>The temperature in ' +
-          query +
-          ' is : ' +
-          temp +
-          ' degrees Celcius.</h3>'
-      );
-      res.write('<img src=' + imageURL + '>');
-      res.send();
-    });
-  });
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+  console.log(firstName, lastName, email);
 });
 
 app.listen(3000, () => {
