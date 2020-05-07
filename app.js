12b08ec9983ae3ff0d596566f6f4baa8deb6a9fa
Andri Sedania <andri@sedaniainnovator.com>
2020-05-07 09:37:02 +0700

    19.252 - Making GET requests with the Node HTTPS module

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const https = require('https');
 
 const app = express();
+const url =
+  'http://api.openweathermap.org/data/2.5/weather?q=Paris&appid=3cbfea738d409eb1692515dd7947cf7b';
 
 app.get('/', (req, res) => {
+  https.get(url, (response) => {
+    console.log(response);
+  });
+
   res.send('Server is up and running');
 });
 
