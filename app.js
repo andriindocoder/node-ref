a2180318cb9bd137145fee0926e88b1eec4c0063
Andri Sedania <andri@sedaniainnovator.com>
2020-05-08 15:29:44 +0700

    Ep.10 - HTTP and API Request

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const rp = require('request-promise');
+const request = require('request');
 
 const app = express();
 
@@ -35,6 +37,27 @@ app.get("/list", (req, res) => {
 app.get("*", (req, res) => {
 	res.send("That page doesn't exist.");
 })
+
+
+// const url = 'https://api.openweathermap.org/data/2.5/weather?q=Bandung&appid=&units=metric';
+const url = 'https://api.unsplash.com/photos?client_id=&order_by=popular&page=1';
+// request(url, function(error, response, body) {
+// 	if(error){
+// 		console.log(error);
+// 	}else{
+// 		var data = JSON.parse(body);
+// 		console.log(data[0].urls.raw);
+// 	}
+// })
+
+rp(url)
+    .then(function (htmlString) {
+    	const data = JSON.parse(htmlString);
+        console.log(data[0].urls.small);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
 
 app.listen(3000, () => {
 	console.log("App is running on port 3000");
