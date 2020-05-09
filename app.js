d41f3f968a98c2bcc9d5ad4702909eb851a24b68
Andri Sedania <andri@sedaniainnovator.com>
2020-05-09 12:57:28 +0700

    Moving files

--- app.js
+++ app.js
@@ -4,9 +4,31 @@ var port = process.env.PORT || 3000;
 var bodyParser = require('body-parser');
 var controller = require('./controller');
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const currentPath = path.join(__dirname, "/public/chart.png");
+const newPath = path.join(__dirname, "/public/images/", "chart.png");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hola');
+  // fs.rename(currentPath, newPath, function(err) {
+  //   if (err) {
+  //     throw err
+  //   } else {
+  //     console.log("Successfully moved the file!")
+  //   }
+  // })
+  try {
+    fs.renameSync(currentPath, newPath)
+    console.log("Successfully moved the file!")
+  } catch(err) {
+    throw err
+  }
+})
 
 const url = 'http://localhost:5555/api/test-node';
 app.get('/analyze', (req, res) => {
