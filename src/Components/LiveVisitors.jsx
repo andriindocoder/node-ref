f8ed1506be3ce7183181f17d643b5dbeaf5085e5
Andri Sedania <andri@sedaniainnovator.com>
2020-05-10 11:05:03 +0700

    5 - React App

---
+++ src/Components/LiveVisitors.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+import {} from 'reactstrap';
+
+class LiveVisitors extends Component {
+	render() {
+		return (
+			<div>
+				Live Visitors
+			</div>
+		);
+	}
+}
+
+export default LiveVisitors;
