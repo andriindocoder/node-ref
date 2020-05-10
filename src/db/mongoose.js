43eb31c26593089d91e12c812f0ce8bdc5289c13
Andri Sedania <andri@sedaniainnovator.com>
2020-05-10 18:22:04 +0700

    11.85 - Data Validation and Sanitization Part I

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 	useUnifiedTopology: true,
@@ -8,10 +9,40 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 const User = mongoose.model('User', {
 	name: {
 		type: String
+		required: true,
+		trim: true,
 	},
 	age: {
-		type: Number
+		type: Number,
+		default: 0,
+		validate(value) {
+			if (value < 0) {
+				throw new Error('Age must be positive number')
+			}
+		} 
+	},
+	email: {
+		type: String,
+		trim: true,
+		required: true,
+		validate(value) {
+			if(!validator.isEmail(value)){
+				throw new Error('Email is invalid')
+			}
+		}
 	}
+})
+
+const me = new User({
+	name: 'Bombo',
+	age: 18
+})
+
+me.save().then(() => {
+	console.log(me)
+})
+.catch((error) => {
+	console.log(error)
 })
 
 const Task = mongoose.model('Task', {
