eefca2819e3c880ca5c92246b4e37814f126db68
Andri Sedania <andri@sedaniainnovator.com>
2020-05-10 16:03:20 +0700

    10.77 - The ObjectID

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,20 @@
 // create read update delete
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+// const ObjectID = mongodb.ObjectID
+
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectID()
+console.log(id)
+console.log(id.getTimestamp())
+console.log(id.id.length)
+console.log(id.toHexString().length)
+
 
 MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client) => {
 	if(error){
@@ -14,8 +24,8 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client)
 	const db = client.db(databaseName)
 
 	// db.collection('users').insertOne({
-	// 	name: 'Andri',
-	// 	age: 38
+	// 	name: 'Vikram',
+	// 	age: 26
 	// }, (error, result) => {
 	// 	if(error){
 	// 		return console.log(error)
@@ -41,24 +51,24 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client)
 	// 	console.log(result.ops)
 	// })
 
-	db.collection('tasks').insertMany([
-	{
-		description : 'Clean the house',
-		completed : true
-	},
-	{
-		description : 'Renew Inspection',
-		completed : false
-	},
-	{
-		description: 'Pot plants',
-		completed: false
-	}
-	], (error, result) => {
-		if(error) {
-			return console.log(error)
-		}
+	// db.collection('tasks').insertMany([
+	// {
+	// 	description : 'Clean the house',
+	// 	completed : true
+	// },
+	// {
+	// 	description : 'Renew Inspection',
+	// 	completed : false
+	// },
+	// {
+	// 	description: 'Pot plants',
+	// 	completed: false
+	// }
+	// ], (error, result) => {
+	// 	if(error) {
+	// 		return console.log(error)
+	// 	}
 
-		console.log(result.ops)
-	})
+	// 	console.log(result.ops)
+	// })
 })
