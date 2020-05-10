1569f4fe6e2cfc888c8fbd3ebaaaceccb19ff584
Andri Sedania <andri@sedaniainnovator.com>
2020-05-10 20:42:12 +0700

    11.91 - Resource Reading Endpoints Part I

--- src/index.js
+++ src/index.js
@@ -20,6 +20,30 @@ app.post('/users', (req, res) => {
 		})
 })
 
+app.get('/users', (req, res) => {
+	User.find({})
+		.then((users) => {
+			res.status(200).send(users)
+		})
+		.catch((error) => {
+			res.status(500).send(error)
+		})
+})
+
+app.get('/users/:id', (req, res) => {
+	const _id = req.params.id
+	User.findById(_id)
+		.then((user) => {
+			if(!user){
+				return res.status(404).send()
+			}
+			res.status(200).send(user)
+		})
+		.catch((error) => {
+			res.status(500).send(error)
+		})
+})
+
 app.post('/tasks', (req, res) => {
 	const task = new Task(req.body)
 
