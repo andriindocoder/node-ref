4387edd91555c8c9bc535be3ffd32c28e32e00dd
Andri Sedania <andri@sedaniainnovator.com>
2020-05-10 11:17:19 +0700

    Base from Buffer

---
+++ app.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const rp = require('request-promise');
+const cors = require('cors');
+const imageToBase64 = require('image-to-base64');
+const puppeteer = require('puppeteer'); 
+
+app.use(express.static("public"));
+app.use(cors());
+
+app.set("view engine", "ejs");
+
+const port = 3001;
+
+app.get('/', (req, res) => {
+	res.send('<h3>Hello World</h3>');
+})
+
+app.get('/analyze', (req, res) => {
+	const url = 'http://localhost:5555/api/test-node';
+	rp(url)
+		.then((data) => {
+			let today = Math.round((new Date()).getTime() / 1000);
+
+			let result = JSON.parse(data);
+			let tanggal = result.map((tanggal) => {
+			  return tanggal.date;
+			});
+			let debit = result.map((debit) => {
+			  return debit.debit
+			});
+			let balance = result.map((balance) => {
+			  return balance.balance
+			});
+			
+			res.render('index', {
+				filename: today,
+				balanceData: balance,
+				spendingData: debit,
+				tanggal: tanggal
+			});
+			// var randomchar = makeid(7);
+			// screenshot('highchart');
+		})
+		.catch((error) => {
+			console.log(error);
+		})
+})
+
+function makeid(length) {
+   var result           = '';
+   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
+   for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
+
+async function screenshot(ssname) {
+	console.log(ssname);
+	const browser = await puppeteer.launch({headless: true});       // run browser
+	const page = await browser.newPage();           // open new tab
+	await page.goto('http://localhost:3001', { waitUntil: 'networkidle2' });          // go to site
+
+	await page.waitForSelector('#container');          // wait for the selector to load
+	const element = await page.$('#container');        // declare a variable with an ElementHandle
+	await element.screenshot({path: __dirname + '/public/images/' + ssname + '.png'}); // take screenshot element in puppeteer
+	await browser.close();
+}
+
+app.get('/image', (req, res) => {
+	var path = __dirname + '/public/images/highchart.png';
+	console.log(path);
+	imageToBase64(path) // you can also to use url
+	    .then(
+	        (response) => {
+	        	res.render('image', {
+	        		base64: JSON.stringify({
+	        			statusCode: 200,
+	        			// data: 'data:image/png;base64,' + response
+	        			data: 'http://localhost:3001/images/highchart.png'
+	        		})
+	        	})
+	            // console.log(response); //cGF0aC90by9maWxlLmpwZw==
+	        }
+	    )
+	    .catch(
+	        (error) => {
+	            console.log(error); //Exepection error....
+	        }
+	    )
+});
+
+app.listen(process.env.PORT || port, () => {
+	console.log('App is running on port: ' + port);
+})
