72f4b8e0592c9be50d387ba88891d5f147b5576f
Andri Sedania <andri@sedaniainnovator.com>
2020-05-11 14:59:38 +0700

    Take a screenshot

--- puppeteer.js
+++ puppeteer.js
@@ -1,10 +1,15 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-	const browser = await puppeteer.launch({headless: false})
+	const browser = await puppeteer.launch({headless: true})
 	const page = await browser.newPage()
 	await page.setViewport({ width: 1280, height: 800 })
 	await page.goto('https://carmalou.com')
+
+	await page.screenshot({
+		path: 'fileshot.png',
+		fullPage: true
+	})
 
 	await browser.close()
 })()
