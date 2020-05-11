9ee381dd947969190d614017183c2008013fa02d
Andri Sedania <andri@sedaniainnovator.com>
2020-05-11 15:14:40 +0700

    Coding tech Youtube - Get list of github repos

--- puppeteer.js
+++ puppeteer.js
@@ -4,10 +4,22 @@ const puppeteer = require('puppeteer');
 	const browser = await puppeteer.launch({headless: true})
 	const page = await browser.newPage()
 	await page.setViewport({ width: 1280, height: 800 })
-	await page.goto('http://localhost:5555/login', {waitUntil: 'networkidle2'})
 
-	var html = await page.content()
-	console.log(html)
+	await page.goto('https://github.com/carmalou?tab=repositories', {waitUntil: 'networkidle2'})
+	await page.waitForSelector('#user-repositories-list li')
+
+	var tmp = await page.evaluate(() => {
+		var repos = document.querySelectorAll('#user-repositories-list li h3 a')
+		return Array.from(repos).map((repo) => { return repo.href })
+	})
+
+	
+	console.log(tmp)
 
 	await browser.close()
 })()
+
+/*
+	page.waitForSelector : waits for the selector to appear in page
+	page.evaluate: accepts a func parameter to be evaluated in the page context
+*/
