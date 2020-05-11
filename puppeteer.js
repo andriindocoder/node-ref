873804ca88b8a2309aa46237baa81e4988aa3bfc
Andri Sedania <andri@sedaniainnovator.com>
2020-05-11 15:40:31 +0700

    pocketadmin.tech screenshot fullpage

--- puppeteer.js
+++ puppeteer.js
@@ -1,32 +1,9 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer');           // include lib
+(async () => {                                    // declare function
+  const browser = await puppeteer.launch();       // run browser
+  const page = await browser.newPage();           // open new tab
+  await page.goto('https://google.com');          // go to site
+  await page.screenshot({path: 'google.png', fullPage: true});    // take screenshot in puppeteer and saving the image to google.png
+  await browser.close();                          // close browser
 
-(async () => {
-	const browser = await puppeteer.launch({headless: true})
-	const page = await browser.newPage()
-
-	await page.goto('https://google.com', {waitUntil: 'networkidle2'})
-
-	await page.pdf({
-		path: 'newpdf.pdf',
-		format: 'Letter',
-		margin: {
-			top: '1in',
-			bottom: '1in',
-			left: '1in',
-			right: '1in'
-		}
-	})
-
-	await browser.close()
-
-
-})()
-
-/*
-	page.waitForSelector : waits for the selector to appear in page
-	page.evaluate: accepts a func parameter to be evaluated in the page context, always return the function and put on variable so you can access it
-	page.focus: accepets a selector and focuses on it
-	page.keyboard.type: simulate keypresses and input text
-	page.click: accepts selector and clicks on it
-	page.waitFor: accepts functions which can prolong wait, if return true, program resumes
-*/
+})();
