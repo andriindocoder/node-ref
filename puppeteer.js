d2b7c33259a598f3e1b3c2f2ea5a8addd52ee0fa
Andri Sedania <andri@sedaniainnovator.com>
2020-05-11 18:57:55 +0700

    to GCP

--- puppeteer.js
+++ puppeteer.js
@@ -2,8 +2,9 @@ const puppeteer = require('puppeteer');         // include library
 
 async function getBunnies() {
 	const browser = await puppeteer.launch({
-		headless: false,
-		defaultViewport: null
+		args : [
+			'--no-sandbox'
+		] 
 	});
 
 	const page = await browser.newPage();
@@ -11,7 +12,6 @@ async function getBunnies() {
 	const url = 'https://nh.craigslist.org/search/sss?query=bunnies&sort=rel&hasPic=1';
 
 	await page.goto(url);
-	console.log('hura');
 
 	await page.waitFor('.result-row');
 	const results = await page.$$eval('.result-row', rows => {
@@ -20,15 +20,24 @@ async function getBunnies() {
 			const titleElement = row.querySelector('.result-title');
 			properties.title = titleElement.innerText;
 			properties.url = titleElement.getAttribute('href');
-			const priceElement = row.querySelector('.result-price');
-			properties.price = priceElement ? priceElement.innerText : '';
+			const priceEl = row.querySelector('.result-price');
+			properties.price = priceEl ? priceEl.innerText : '';
+			const imageEl = row.querySelector('.swipe [data-index="0"] img');
+			properties.imageUrl = imageEl ? imageEl.src : '';
 			return properties;
 		});
 	});
 
+	browser.close();
 	console.log(results);
 
-	browser.close();
+	// return {
+	// 	url,
+	// 	results
+	// }
 }
 
 getBunnies();
+// exports.getBunnies = async function(req, res){
+// 	res.status(200).send(await getBunnies());
+// }
