c9186cc41aa1bcdfd402b039d360b4791150ec09
Andri Sedania <andri@sedaniainnovator.com>
2020-05-11 15:42:48 +0700

    pocketadmin.text screenshot area

--- puppeteer.js
+++ puppeteer.js
@@ -1,12 +1,17 @@
-const puppeteer = require('puppeteer');           // include lib
-(async () => {                                    // declare function
-  const browser = await puppeteer.launch();       // run browser
-  const page = await browser.newPage();           // open new tab
-  await page.goto('https://google.com');          // go to site
+const puppeteer = require('puppeteer');
+(async () => {                                      // declare function
+  const browser = await puppeteer.launch();         // run browser
+  const page = await browser.newPage();             // open new tab
+  await page.goto('https://google.com');            // go to site
 
-  // Далее #hplogo - требуемый нам селектор
-  await page.waitForSelector('#hplogo');          // wait for the selector to load
-  const element = await page.$('#hplogo');        // declare a variable with an ElementHandle
-  await element.screenshot({path: 'google.png'}); // take screenshot element in puppeteer
-  await browser.close();                          // close browser
+  // #hplogo - selector
+  await page.waitForSelector('#hplogo');             // wait for the selector to load
+  const logo = await page.$('#hplogo');              // declare a variable with an ElementHandle
+  const box = await logo.boundingBox();              // this method returns an array of geometric parameters of the element in pixels.
+  const x = box['x'];                                // coordinate x
+  const y = box['y'];                                // coordinate y
+  const w = box['width'];                            // area width
+  const h = box['height'];                           // area height
+  await page.screenshot({'path': 'logo.png', 'clip': {'x': x, 'y': y, 'width': w, 'height': h}});     // take screenshot of the required area in puppeteer
+  await browser.close();                             // close browser
 })();
