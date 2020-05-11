aef8453c26beec16b1a6abbccbcf7e436d156e36
Andri Sedania <andri@sedaniainnovator.com>
2020-05-11 16:35:54 +0700

    pocketadmin pdf 1

--- puppeteer.js
+++ puppeteer.js
@@ -1,17 +1,10 @@
-const puppeteer = require('puppeteer');
-(async () => {                                      // declare function
-  const browser = await puppeteer.launch();         // run browser
-  const page = await browser.newPage();             // open new tab
-  await page.goto('https://google.com');            // go to site
+const puppeteer = require('puppeteer');         // include library
 
-  // #hplogo - selector
-  await page.waitForSelector('#hplogo');             // wait for the selector to load
-  const logo = await page.$('#hplogo');              // declare a variable with an ElementHandle
-  const box = await logo.boundingBox();              // this method returns an array of geometric parameters of the element in pixels.
-  const x = box['x'];                                // coordinate x
-  const y = box['y'];                                // coordinate y
-  const w = box['width'];                            // area width
-  const h = box['height'];                           // area height
-  await page.screenshot({'path': 'logo.png', 'clip': {'x': x, 'y': y, 'width': w, 'height': h}});     // take screenshot of the required area in puppeteer
-  await browser.close();                             // close browser
+(async () => {                                  // declare function
+  const browser = await puppeteer.launch();     // run browser
+  const page = await browser.newPage();         // create new tab
+  await page.goto('https://google.com');  // go to page
+  await page.emulateMedia('screen');            // use screen media
+  await page.pdf({path: 'page.pdf', displayHeaderFooter: true, printBackground: true});  // generate pdf
+  await browser.close();                        // close browser
 })();
