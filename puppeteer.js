c0485a9573ffcde1b1d159e9c895b36eaed65bc9
Andri Sedania <andri@sedaniainnovator.com>
2020-05-12 15:33:46 +0700

    Hitesh Youtube - Page Evalute

--- puppeteer.js
+++ puppeteer.js
@@ -1,43 +1,21 @@
-const puppeteer = require('puppeteer');         // include library
+const puppeteer = require('puppeteer');
 
-async function getBunnies() {
-	const browser = await puppeteer.launch({
-		args : [
-			'--no-sandbox'
-		] 
-	});
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://en.wikipedia.org/wiki/Coronavirus');
+  await page.screenshot({path: 'wiki.png'});
+  // await browser.waitForTarget(() => false)
 
-	const page = await browser.newPage();
+  // Get the "viewport" of the page, as reported by the page.
+    const result = await page.evaluate(() => {
+      let headingWiki = document.querySelectorAll(".mw-headline");
+      const headingList = [...headingWiki];
+      return headingList.map(h => h.innerText);
+    });
 
-	const url = 'https://nh.craigslist.org/search/sss?query=bunnies&sort=rel&hasPic=1';
+    console.log(result);
 
-	await page.goto(url);
 
-	await page.waitFor('.result-row');
-	const results = await page.$$eval('.result-row', rows => {
-		return rows.map(row => {
-			const properties = {};
-			const titleElement = row.querySelector('.result-title');
-			properties.title = titleElement.innerText;
-			properties.url = titleElement.getAttribute('href');
-			const priceEl = row.querySelector('.result-price');
-			properties.price = priceEl ? priceEl.innerText : '';
-			const imageEl = row.querySelector('.swipe [data-index="0"] img');
-			properties.imageUrl = imageEl ? imageEl.src : '';
-			return properties;
-		});
-	});
-
-	browser.close();
-	console.log(results);
-
-	// return {
-	// 	url,
-	// 	results
-	// }
-}
-
-getBunnies();
-// exports.getBunnies = async function(req, res){
-// 	res.status(200).send(await getBunnies());
-// }
+  await browser.close();
+})();
