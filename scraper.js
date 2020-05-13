f195ad7d68cde474b3e753d2485e6e55f76acac5
Andri Sedania <andri@sedaniainnovator.com>
2020-05-13 11:12:24 +0700

    Youtube devtips : Change to a function

--- scraper.js
+++ scraper.js
@@ -1,29 +1,24 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://marketingplatform.google.com/about/partners/find-a-partner');
-  // await page.screenshot({path: 'example.png'});
-  await page.waitForSelector('div.retail-partner-card h3.title');
+  const extractPartners = async (url) => {
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.waitForSelector('div.retail-partner-card h3.title');
+    const partners = await page.evaluate(() => 
+      Array.from(document.querySelectorAll('div.retail-partner-card'))
+        .map(compact => ({
+          title: compact.querySelector('h3.title').textContent.trim(),
+          logo: compact.querySelector('.logo img').src
+        })))
+    return partners
+  }
+  const browser = await puppeteer.launch();  
 
-  const titles = await page.evaluate(() => 
-  	Array.from(document.querySelectorAll('div.retail-partner-card h3.title'))
-      .map(partner => partner.innerText.trim()))
+  const url = 'https://marketingplatform.google.com/about/partners/find-a-partner';
 
-  const logos = await page.evaluate(() => 
-    Array.from(document.querySelectorAll('div.retail-partner-card .logo img'))
-      .map(logo => logo.src))
+  const partners = await extractPartners(url);
 
-  const partners = await page.evaluate(() => 
-    Array.from(document.querySelectorAll('div.retail-partner-card'))
-      .map(compact => ({
-        title: compact.querySelector('h3.title').textContent.trim(),
-        logo: compact.querySelector('.logo img').src
-      })))
-
-  console.log(titles);
-  console.log(logos);
   console.log(partners);
 
   await browser.close();
