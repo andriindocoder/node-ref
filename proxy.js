b21adc10026461c881d9508ef921f08a3185ac16
Andri Sedania <andri@sedaniainnovator.com>
2020-05-16 13:02:21 +0700

    Using proxy

--- proxy.js
+++ proxy.js
@@ -1,12 +1,17 @@
 const fs = require('fs')
-const request = require('request-promise')
+const rp = require('request-promise')
 
 async function main() {
-	const result = await request.get(
-		"https://indocoder.com"
-	);
-
-	fs.writeFileSync("./test.html", result);
+	const result = rp({
+	            resolveWithFullResponse: true,
+	            uri: 'https://indocoder.com',
+	            agentOptions: {
+	                socksHost: "180.210.201.57", // Defaults to 'localhost'.
+	                socksPort: 3130 // Defaults to 1080.
+	            }
+	        }).then((hasil) => {
+				fs.writeFileSync("./test4.html", hasil.body);
+	        })
 }
 
 main();
