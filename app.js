fdb30b88f29d18e5a8aa527e1f9d0b7ff8c19554
Andri Sedania <andri@sedaniainnovator.com>
2020-05-16 10:53:29 +0700

    Flash session message

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
+const flash = require('connect-flash')
+const session = require('express-session')
 
 const app = express()
 
 //DB Config
 const db = require('./config/keys').MongoURI
-
-// mongoose.connect(db, { useUnifiedTechnology: true}, (error) => {
-// 	if(error){
-// 		console.log(error);
-// 	}else{
-// 		console.log("Database Connected");
-// 	}
-// })
 
 mongoose.connect(db, {
     useUnifiedTopology: true, useNewUrlParser: true
@@ -31,6 +25,23 @@ app.set('view engine', 'ejs')
 
 //Body parser
 app.use(express.urlencoded({ extended: false }))
+
+//Express
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}))
+
+// Connect flash
+app.use(flash())
+
+// Global vars
+app.use((req, res, next) =>  {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.error_msg = req.flash('error_msg')
+  next()
+})
 
 //Routes
 app.use('/', require('./routes/index'))
