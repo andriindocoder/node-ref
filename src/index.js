510c1780dd67b0bb4801862a5502f2cd93597890
Andri Sedania <andri@sedaniainnovator.com>
2020-05-17 20:50:53 +0700

    12.144 The User/Task Relationship

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
+const mongoose = require('mongoose')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -13,3 +14,18 @@ app.use(taskRouter)
 app.listen(port, () => {
 	console.log('Server is running on port ' + port)
 })
+
+const Task = require('./models/task')
+const User = require('./models/User')
+
+const main = async() => {
+	// const task = await Task.findById('5ec13a0f28f6de1546fa1ad7')
+	// await task.populate('owner').execPopulate()
+	// console.log(task.owner)
+
+	const user = await User.findById('5ec137e1938d06147191ee1b')
+	await user.populate('tasks_user').execPopulate()
+	console.log(user.tasks_user)
+}
+
+main()
