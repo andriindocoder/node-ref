48111ec2b8944810bfdf85c26e5f5bf1ba9389ed
Andri Sedania <andri@sedaniainnovator.com>
2020-05-17 12:18:13 +0700

    Wiki portrait landscape

--- puppeteer.js
+++ puppeteer.js
@@ -13,7 +13,7 @@ const puppeteer = require('puppeteer');         // include library
   //       `;
   // const navigationPromise = page.waitForNavigation()
     
-  await page.goto('https://www.nytimes.com/2020/05/15/briefing/11-of-our-best-weekend-reads.html?action=click&module=Top%20Stories&pgtype=Homepage', {waitUntil: 'networkidle2', timeout: 0})
+  await page.goto('https://en.wikipedia.org/wiki/Fiverr', {waitUntil: 'networkidle2', timeout: 0})
   // await page.waitForFunction("document.querySelector('img') && document.querySelector('img').clientHeight != 0");
   // or wait until "visibility" not hidden
   // await page.waitForFunction("document.querySelector('img') && document.querySelector('img').style.visibility != 'hidden'");
@@ -34,22 +34,22 @@ const puppeteer = require('puppeteer');         // include library
 
   await page.emulateMedia('screen');            // use screen media
   await page.pdf({
-        path: 'nytimes.pdf',                       // path to save pdf file  
+        path: 'wiki_portrait.pdf',                       // path to save pdf file  
         displayHeaderFooter: false,              // display header and footer (in this example, required!)
         printBackground: true,                  // print background
         // landscape: false,                        // use horizontal page layout
         landscape: false,                        // use horizontal page layout
         // headerTemplate: html,                   // indicate html template for header
         margin: {                               // increase margins (in this example, required!)
-          top: 80,
-          bottom: 80,
-          left: 30,
-          right: 30
+          top: 40,
+          bottom: 40,
+          left: 15,
+          right: 15
           }
         });
   setTimeout(async function() {
           await page.pdf({
-                path: 'nytimes2.pdf',                       // path to save pdf file  
+                path: 'wiki_landscape.pdf',                       // path to save pdf file  
                 displayHeaderFooter: false,              // display header and footer (in this example, required!)
                 printBackground: true,                  // print background
                 // landscape: false,                        // use horizontal page layout
